e2db963ca58581babe7e745ea2c5b3e91fdd8ad0
caohenghu <caohenghu@gmail.com>
2022-12-13 20:30:23 +0800

    feat: 调整逻辑

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  semi: false, // 不使用分号
+  singleQuote: true, // 使用单引号
+  trailingComma: "none", // 不需要尾逗号
+  arrowParens: "avoid", // 箭头函数单个参数时不使用括号
+  vueIndentScriptAndStyle: true, // vue文件中script和style里使用缩进
+};
